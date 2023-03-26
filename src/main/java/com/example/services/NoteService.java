1b1812c839a7eb84b6bbbdbf10ee029df6855659
sergeyonischuk <sergey.onischuk@ukr.net>
2023-03-26 13:50:12 +0300

    service methods updated

--- src/main/java/com/example/services/NoteService.java
+++ src/main/java/com/example/services/NoteService.java
@@ -3,29 +3,46 @@ package com.example.services;
 import com.example.entities.Note;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class NoteService {
-    private List<Note> noteList;
+    private Map<Long,Note> noteList = new HashMap<>();
 
     public List<Note> listAll() {
-        return noteList;
+        return new ArrayList<>(noteList.values());
     }
 
     public Note add(Note note) {
-        return null;
+        Long id = new Random().nextLong();
+        while (noteList.containsKey(id)) {
+            id = new Random().nextLong();
+        }
+        note.setId(id);
+        noteList.put(id, note);
+        return note;
     }
 
     public void deleteById(Long id) {
-
+        if (!noteList.containsKey(id)) {
+            throw new IllegalArgumentException("Note with id " + id + " does not exist.");
+        }
+        Note note = noteList.get(id);
+        noteList.remove(id, note);
     }
 
     public void update(Note note) {
-
+        if (!noteList.containsValue(note)) {
+            throw new IllegalArgumentException("Note with id " + note.getId() + " does not exist.");
+        }
+        noteList.get(note.getId()).setTittle(note.getTittle());
+        noteList.get(note.getId()).setContent(note.getContent());
     }
 
     public Note getById(Long id) {
-        return null;
+        if (!noteList.containsKey(id)) {
+            throw new IllegalArgumentException("Note with id " + id + " does not exist.");
+        }
+        return noteList.get(id);
     }
 }
